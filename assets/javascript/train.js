17939eecbd203dc0fb6c7bbf68b3063aa9102274
Achille Bavoua <bavouasoft@yahoo.fr>
2020-05-14 11:44:46 -0400

    clean code

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -6,7 +6,7 @@
 //
 //
 
-//Start with entering first: train name/destination/first train time/
+//Start with entering first: train name/destination/
 //initial .on("click") should generate new html rows
 //containing  train name / destination / frequency / Next arrival / number of minutes away
 //next arrival and to the destination will be calculated on the basis of frequency entered
